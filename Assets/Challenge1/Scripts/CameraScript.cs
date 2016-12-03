73bbb332b3ed7be9344d16ada06918c17f02c237
Laurent Aymard <laurent.aymard@insa-rennes.fr>
2016-12-04 00:38:23 +0100

    character turn in right direction

--- Assets/Challenge1/Scripts/CameraScript.cs
+++ Assets/Challenge1/Scripts/CameraScript.cs
@@ -3,28 +3,27 @@ using System.Collections;
 
 public class CameraScript : MonoBehaviour {
 
-   private Camera camera;
+   private Camera p_camera;
     public Transform player;
 
 	// Use this for initialization
 	void Start () {
-        camera = this.GetComponent<Camera>();
+        p_camera = this.GetComponent<Camera>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log(Input.GetAxisRaw("Mouse X"));
 
         if (Input.GetAxisRaw("Mouse X") != 0)
         {
-            camera.transform.RotateAround(player.position, player.up, 2.0f * Input.GetAxisRaw("Mouse X"));
+            p_camera.transform.RotateAround(player.position, player.up, 2.0f * Input.GetAxisRaw("Mouse X"));
         }
         if (Input.GetAxisRaw("Mouse Y") != 0)
         {
-            camera.transform.RotateAround(player.position, player.right, 2.0f * Input.GetAxisRaw("Mouse Y"));
+            p_camera.transform.RotateAround(player.position, player.right, 2.0f * Input.GetAxisRaw("Mouse Y"));
         }
 
-        camera.transform.LookAt(player);
+        p_camera.transform.LookAt(player);
 
     }
 }
--- Assets/Challenge1/Scripts/PlayerController.cs
+++ Assets/Challenge1/Scripts/PlayerController.cs
@@ -1,43 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-    private Transform transform;
-    public Camera camera;
+    private Transform p_transform;
+    public Camera p_camera;
 
-	// Use this for initialization
-	void Start () {
-        transform = this.GetComponent<Transform>();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+    // Use this for initialization
+    void Start()
+    {
+        p_transform = this.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         if (Input.GetAxisRaw("Vertical") != 0)
         {
             // update orientation from camera
-            Vector3 orien = transform.position - camera.transform.position;
+            Vector3 orien = p_transform.position - p_camera.transform.position;
             orien.Normalize();
+            Vector3 cleft = -p_camera.transform.right;
+            Vector2 cleft2 = new Vector2(cleft.x, cleft.z);
 
             Vector2 a = new Vector2(orien.x, orien.z);
             a.Normalize();
-            Vector2 b = new Vector2(transform.forward.x, transform.forward.z);
+            Vector2 b = new Vector2(p_transform.forward.x, p_transform.forward.z);
             b.Normalize();
 
+            float sign = Vector2.Dot(b, cleft2);
+            if (sign > 0)
+            {
+                sign = 1f;
+            }
+            else
+            {
+                sign = -1f;
+            }
+
             float cos = Vector2.Dot(a, b);
+            orien.y = 0;
+            orien.Normalize();
+            Vector3 cr = Vector3.Cross(orien, p_transform.forward);
+
+           
+            if (cos > 1) cos = 1f;
+            if (cos < -1) cos = -1f;
             float arccos = Mathf.Acos(cos);
-            arccos=Mathf.Rad2Deg*arccos;
+            arccos = Mathf.Rad2Deg * arccos;
 
-            Debug.Log("Arcos : "+arccos);
 
-            transform.RotateAround(transform.position, transform.up, arccos);
-            camera.transform.RotateAround(transform.position, transform.up, -arccos);
+            p_transform.RotateAround(p_transform.position, p_transform.up, 0.1f * arccos*sign);
+            p_camera.transform.RotateAround(p_transform.position, p_transform.up, -arccos * 0.1f*sign  );
 
-            transform.position += transform.forward*0.01f;
+            p_transform.position += p_transform.forward * 0.01f;
 
         }
-	}
+    }
 
 
 }
