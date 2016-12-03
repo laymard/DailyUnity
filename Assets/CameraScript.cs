1e973a44f47f8147d5a1f8466b51f43ca613a6da
Laurent Aymard <laurent.aymard@insa-rennes.fr>
2016-12-03 23:36:58 +0100

    init commit

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+   private Camera camera;
+    public Transform player;
+
+	// Use this for initialization
+	void Start () {
+        camera = this.GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(Input.GetAxisRaw("Mouse X"));
+
+        if (Input.GetAxisRaw("Mouse X") != 0)
+        {
+            camera.transform.RotateAround(player.position, player.up, 2.0f * Input.GetAxisRaw("Mouse X"));
+        }
+        if (Input.GetAxisRaw("Mouse Y") != 0)
+        {
+            camera.transform.RotateAround(player.position, player.right, 2.0f * Input.GetAxisRaw("Mouse Y"));
+        }
+
+        camera.transform.LookAt(player);
+
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Transform transform;
+    public Camera camera;
+
+	// Use this for initialization
+	void Start () {
+        transform = this.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (Input.GetAxisRaw("Vertical") != 0)
+        {
+            // update orientation from camera
+            Vector3 orien = transform.position - camera.transform.position;
+            orien.Normalize();
+
+            Vector2 a = new Vector2(orien.x, orien.z);
+            a.Normalize();
+            Vector2 b = new Vector2(transform.forward.x, transform.forward.z);
+            b.Normalize();
+
+            float cos = Vector2.Dot(a, b);
+            float arccos = Mathf.Acos(cos);
+            arccos=Mathf.Rad2Deg*arccos;
+
+            Debug.Log("Arcos : "+arccos);
+
+            transform.RotateAround(transform.position, transform.up, arccos);
+            camera.transform.RotateAround(transform.position, transform.up, -arccos);
+
+            transform.position += transform.forward*0.01f;
+
+        }
+	}
+
+
+}
